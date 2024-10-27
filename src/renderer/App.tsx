2a9deb607c15bb39176529d6bb99085d455b765b
shouzou-nozaki <soro0530@icloud.com>
2024-10-28 06:31:37 +0900

    サービスクラス作成前分

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,12 +2,8 @@ import { useState, useEffect } from 'react';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
-// データ型を定義
-interface Todo {
-  id: number;
-  text: string;
-  completed: boolean;
-}
+// 検索欄の値
+let searchText:string;
 
 export default function App() {
   return (
@@ -33,13 +29,20 @@ const HomeScreen = () => {
   // 検索画面
   return (
     <div>
-      <div className="logo">File<span>Probe</span></div>
+    <div className="logo">File<span>Probe</span></div>
 
-      <div className="search-box">
-        <input className='search-text' type="text" placeholder="検索ファイル名を入力"/>
-        <button onClick={SearchFile}>検索</button>
-      </div>
+    <div className="search-box">
+      <input 
+        className='search-text'
+        type="text"
+        placeholder="検索ファイル名を入力"
+        value = {searchText}
+        onChange={(e) => searchText = e.target.value} // 入力のたびにsearchTextを更新
+        />
+      <button onClick={SearchFile}>検索</button>
     </div>
+  </div>
+
   );
 };
 
